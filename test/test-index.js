e218cbf2349e76cf70c817e04be801f44fe73fd2
Puton1221 <areas.ruler@gmail.com>
2023-03-17 19:26:34 +0900

    独自エラーの追加およびdataオブジェクト作成時のおかしい点を修正

--- test/test-index.js
+++ test/test-index.js
@@ -3,6 +3,7 @@ const makeJson = require('../lib/makeJson.js');
 const dotenv = require('dotenv');
 dotenv.config();
 test("makeJson complete", async t => {
-    const result = await makeJson(process.env.uid, "雷電将軍", "ATTACK")
+    const result = await makeJson(process.env.uid, "雷電将軍", "ATTACK");
+    console.dir(result, { depth: null })
     t.pass();
 });
