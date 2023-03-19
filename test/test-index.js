31b262757865641a906cedcd2c6f72789c944365
Puton1221 <areas.ruler@gmail.com>
2023-03-20 05:21:15 +0900

    開発中

--- test/test-index.js
+++ test/test-index.js
@@ -2,8 +2,11 @@ const test = require('ava');
 const makeJson = require('../lib/makeJson.js');
 const dotenv = require('dotenv');
 dotenv.config();
+
+// 神里綾華
+
 test("makeJson complete", async t => {
-    const result = await makeJson(process.env.uid, "雷電将軍", "ATTACK");
-    console.dir(result, { depth: null })
+    const result = await makeJson(process.env.uid, "胡桃", "ATTACK");
+    console.log(result)
     t.pass();
 });
