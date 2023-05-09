e8826dc2f1a4856c227c1be47dcf3114a3be38ea
Puton1221 <areas.ruler@gmail.com>
2023-05-10 08:43:49 +0900

    cacheDirectoryを指定しないときにエラーが出る問題を修正

--- test/test-index.js
+++ test/test-index.js
@@ -3,8 +3,6 @@ const makeJson = require('../lib/makeJson.js');
 const dotenv = require('dotenv');
 dotenv.config();
 
-// 神里綾華
-
 test("makeJson complete", async t => {
     const result = await makeJson(process.env.uid, "雷電将軍", "ATTACK", { cacheDirectory: "./cache" });
     console.dir(result, { depth: null })
