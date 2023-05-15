51da7336de8a6c0a463d2f2cbc05d8457090ee24
Puton1221 <areas.ruler@gmail.com>
2023-05-16 06:50:44 +0900

    キャッシュシステムの修正

--- test/test-index.js
+++ test/test-index.js
@@ -1,10 +1,10 @@
 const test = require('ava');
-const makeJson = require('../lib/makeJson.js');
+const makeJson = require('../index.js');
 const dotenv = require('dotenv');
 dotenv.config();
 
 test("makeJson complete", async t => {
     const result = await makeJson(process.env.uid, "雷電将軍", "ATTACK", { cacheDirectory: "./cache" });
-    console.dir(result, { depth: null })
+    console.dir(result, { depth: null });
     t.pass();
 });
